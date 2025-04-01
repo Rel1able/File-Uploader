4a92083dce0fe61bfa2794297cc690c2d5533246
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-04-01 17:06:02 +0200

    fixed route

--- routes/filesRouter.js
+++ routes/filesRouter.js
@@ -8,6 +8,7 @@ filesRouter.get("/upload-file", (req, res, next) => {
     res.render("uploadFileForm");
 })
 
+filesRouter.post("/upload-file", upload.single("file"), filesController.handleUpload )
 filesRouter.post("/folders/:id/upload-file", upload.single("file"), filesController.handleUpload)
 
 
