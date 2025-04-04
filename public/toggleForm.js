3595e882fdb4f9ea5b55b506124f3fb73f2d3b4d
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-04-04 14:05:44 +0200

    fixed issue that form for file uploading didn't appear inside of a folder

--- public/toggleForm.js
+++ public/toggleForm.js
@@ -3,21 +3,33 @@
 let uploadFileBtn = document.querySelector(".upload-btn");
 let uploadFileForm = document.querySelector(".upload-file-form");
 
+
+
+if (uploadFileBtn && uploadFileForm) {
+    uploadFileBtn.addEventListener("click", () => {
+        
+        let createFolderForm = document.querySelector(".create-folder-form");
+        if (createFolderForm) {
+             createFolderForm.classList.remove("visible")
+        }
+       
+        uploadFileForm.classList.toggle("visible");
+
+    
+    })
+}
+
 let createFolderBtn = document.querySelector(".create-folder-btn");
 let createFolderForm = document.querySelector(".create-folder-form");
 
-uploadFileBtn.addEventListener("click", () => {
-    createFolderForm.classList.remove("visible")
-    uploadFileForm.classList.toggle("visible");
+if (createFolderBtn && createFolderForm) {
+    createFolderBtn.addEventListener("click", () => {
+        uploadFileForm.classList.remove("visible");
+        createFolderForm.classList.toggle("visible");
+    })
+}
 
-    
-})
 
-
-createFolderBtn.addEventListener("click", () => {
-    uploadFileForm.classList.remove("visible");
-    createFolderForm.classList.toggle("visible");
-})
 
 
 
