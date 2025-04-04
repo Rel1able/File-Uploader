a1632e785fe0f9e9cb35bcde0ef6ced4f3d6c93f
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-04-04 09:34:09 +0200

    made create folder form appear on click

--- public/toggleForm.js
+++ public/toggleForm.js
@@ -1,5 +1,17 @@
 let uploadBtn = document.querySelector(".upload-btn");
 let form = document.querySelector(".upload-file-form");
+
+let createFolderBtn = document.querySelector(".create-folder-btn");
+let createFolderForm = document.querySelector(".create-folder-form");
+
 uploadBtn.addEventListener("click", () => {
     form.classList.toggle("visible");
 })
+
+
+createFolderBtn.addEventListener("click", () => {
+    createFolderForm.classList.toggle("visible");
+})
+
+
+
